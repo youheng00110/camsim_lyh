6c112d724a3420919e75427df93bc583e2480686
youheng00110 <25303070058@m.fudan.edu.cn>
2026-04-24 17:07:50 +0800

    给diffusionforcing加入了refframe

--- OpenDWM/src/dwm/train.py
+++ OpenDWM/src/dwm/train.py
@@ -22,7 +22,7 @@ def create_parser():
         "--log-steps", default=300, type=int,
         help="The step count to print log and update the tensorboard.")
     parser.add_argument(
-        "--preview-steps", default=900, type=int,
+        "--preview-steps", default=800, type=int,
         help="The step count to preview the pipeline result.")
     parser.add_argument(
         "--checkpointing-steps", default=7000, type=int,
