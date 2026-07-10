7aa4e9e47ef7905c52608ad4c5c8cc43bdd35755
youheng00110 <25303070058@m.fudan.edu.cn>
2026-07-10 11:36:39 +0800

    训练waymoing,加了可以筛选不同指标下差异最大的方法的逻辑

--- run_waymotoken.sh
+++ run_waymotoken.sh
@@ -1,9 +1,10 @@
 source /inspire/ssd/project/advanced-machine-learning/public/inspire_shared/envs/lyhdwm/bin/activate
 cd /inspire/qb-ilm/project/advanced-machine-learning/yanjunchi-24040/camsim_lyh/OpenDWM/src
 
+
 unset ENABLE_DEBUGPY
 unset DEBUGPY_PORT
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export OMP_NUM_THREADS=1
 export TOKENIZERS_PARALLELISM=false
 export PYTHONUNBUFFERED=1
@@ -12,11 +13,11 @@ export PYTHONPATH=/inspire/qb-ilm/project/advanced-machine-learning/yanjunchi-24
 export PYTHONPATH=/inspire/qb-ilm/project/advanced-machine-learning/yanjunchi-24040/camsim_lyh/nuplan-devkit-master:$PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:/inspire/qb-ilm/project/advanced-machine-learning/yanjunchi-24040/camsim_lyh/OpenDWM/externals/waymo-open-dataset/src
 torchrun \
-  --nproc_per_node=4 \
+  --nproc_per_node=8 \
   -m dwm.train \
-  -c //inspire/qb-ilm/project/advanced-machine-learning/yanjunchi-24040/camsim_lyh/OpenDWM/configs/ctsd/unimlvg/camsim/waymoplucker.json\
-  -o /inspire/qb-ilm/project/advanced-machine-learning/yanjunchi-24040/camsim_lyh/output/train_waymoplucker\
-  --log-steps  150 \
-  --preview-steps 400 \
-  --checkpointing-steps 5000 \
-  --evaluation-steps 5000 \
+  -c /inspire/qb-ilm/project/advanced-machine-learning/yanjunchi-24040/camsim_lyh/OpenDWM/configs/ctsd/unimlvg/camsim/waymotoken.json\
+  -o /inspire/qb-ilm/project/advanced-machine-learning/yanjunchi-24040/camsim_lyh/output/train_waymotoken\
+  --log-steps  500 \
+  --preview-steps 1000 \
+  --checkpointing-steps 6000 \
+  --evaluation-steps 200000
